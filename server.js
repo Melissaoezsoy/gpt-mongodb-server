73c7fba0885d4a18764fbfcbc78423c1643c9059
Melissaoezsoy <oezsoy.melissa@gmail.com>
2025-04-06 18:45:12 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const { MongoClient } = require("mongodb");
 const path = require("path");
-const { Configuration, OpenAIApi } = require("openai");
+const OpenAI = require("openai");
 
 const app = express();
 const port = process.env.PORT || 3000;
